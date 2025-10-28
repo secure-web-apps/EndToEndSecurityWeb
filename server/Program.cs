d6ab55f701c2a241f2f80bf7994d5a5cfbff320c
Marc Rufer <marc.rufer@isolutions.ch>
2025-10-28 23:15:31 +0100

    Add integration test

--- server/Program.cs
+++ server/Program.cs
@@ -108,3 +108,11 @@ if (app.Environment.IsDevelopment())
 app.MapFallbackToPage("/_Host");
 
 await app.RunAsync();
+
+/// <summary>
+/// Expose implicitly defined Program class to the integration test project
+/// See https://learn.microsoft.com/en-us/aspnet/core/test/integration-tests?view=aspnetcore-9.0&pivots=xunit&WT.mc_id=MVP_344197#basic-tests-with-the-default-webapplicationfactory
+/// </summary>
+#pragma warning disable S1118 // Utility classes should not have public constructors
+public partial class Program { }
+#pragma warning restore S1118 // Utility classes should not have public constructors
---
+++ tests/UserEndpointsTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net.Http.Json;
+
+namespace BffMicrosoftEntraID.Server.IntegrationTests;
+
+public class UserEndpointsTests : IClassFixture<WebApplicationFactory<Program>>
+{
+    private readonly WebApplicationFactory<Program> _factory;
+
+    public UserEndpointsTests(WebApplicationFactory<Program> factory)
+    {
+        _factory = factory;
+    }
+
+    [Fact]
+    public async Task Get_AsUnauthenticatedUser_ReturnsAnonymousUserInfo()
+    {
+        // Arrange
+        var client = _factory.CreateClient();
+
+        // Act
+        var response = await client.GetAsync("/api/user");
+
+        // Assert
+        response.EnsureSuccessStatusCode();
+        var userInfo = await response.Content.ReadFromJsonAsync<Models.UserInfo>();
+        Assert.NotNull(userInfo);
+        Assert.False(userInfo!.IsAuthenticated);
+    }
+}

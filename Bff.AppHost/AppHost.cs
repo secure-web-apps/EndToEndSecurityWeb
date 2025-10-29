d30b347df372656d6eabfb6afaa8bee8baffc11c
Marc Rufer <marc.rufer@isolutions.ch>
2025-10-29 13:31:49 +0100

    Add additional link to README and remove commented out code

--- Bff.AppHost/AppHost.cs
+++ Bff.AppHost/AppHost.cs
@@ -5,7 +5,6 @@ var builder = DistributedApplication.CreateBuilder(args);
 if (builder.Environment.IsDevelopment())
 {
     var angularFrontend = builder.AddNpmApp("angular", "../ui", "start")
-        //.WithEnvironment("VITE_API_URL", apiService.GetEndpoint("api"))
         .WithHttpsEndpoint(port: 3000, 4201, env: "BASE_URL");
 
     builder.AddProject<Projects.BffMicrosoftEntraID_Server>("bffmicrosoftentraid-server")

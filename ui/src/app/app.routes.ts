20b63021543037152bae6dbeabf9ae36d60fa07a
Marc Rufer <marc.rufer@isolutions.ch>
2025-10-30 15:03:11 +0100

    Revert last commit

--- ui/src/app/app.routes.ts
+++ ui/src/app/app.routes.ts
@@ -1,7 +1,3 @@
 import { Route } from '@angular/router';
-import { HomeComponent } from './home.component';
 
-export const appRoutes: Route[] = [
-    { path: '', component: HomeComponent },
-    { path: '**', redirectTo: '' }
-];
+export const appRoutes: Route[] = [];

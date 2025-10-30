ac8148780f21bdcaf3604113ede9ba8b0d8ba9e7
Marc Rufer <marc.rufer@isolutions.ch>
2025-10-30 13:46:41 +0100

    Set up proper routing

--- ui/src/app/app.routes.ts
+++ ui/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Route } from '@angular/router';
+import { HomeComponent } from './home.component';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+    { path: '', component: HomeComponent },
+    { path: '**', redirectTo: '' }
+];
